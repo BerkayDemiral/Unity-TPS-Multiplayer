53a48d9b40d39cbcedd2e986ae1da77d771ceaa7
Berkay <61667359+BerkayDemiral@users.noreply.github.com>
2025-11-07 23:57:32 +0300

    feat: add server manager for hosting and session control
    - Implemented ServerManager to handle hosting and client connections
    - Integrated with matchmaking and SessionManager for server creation
    - Added logic for initializing and shutting down game sessions

---
+++ Server Manager/Terminal.cs
@@ -0,0 +1,127 @@
+﻿using System;
+
+namespace DevelopersHub.RealtimeNetworking.Server
+{
+    class Terminal
+    {
+
+        public const int port = 7777;
+        public const int updates_per_second = 30;
+        public const int max_players = 100000;
+        public const string sqlite_database_file_path = @"C:\Database\UnityTPSMultiplayer.db";
+        public const string log_directory_path = @"C:\Log\realtime_networking\";
+
+        #region Extensions
+        public const string netcode_server_executable_path = @"C:\Users\Berkay\Desktop\Unity Online\Netcode Server\UnityTPSMultiplayer.exe";
+        public const int netcode_max_server_life_seconds = 21600;
+        #endregion
+
+        public static void Start()
+        {
+            Console.WriteLine("Server Started.");
+        }
+
+        public static void Update()
+        {
+
+        }
+
+        public static void ClientConnected(int id, string ip)
+        {
+
+        }
+
+        public static void ClientDisconnected(int id, string ip)
+        {
+
+        }
+
+        public static void PacketReceived(int clientID, Packet packet)
+        {
+
+        }
+
+        public static void OnAuthenticated(long accountID, bool wasSignedUp, Microsoft.Data.Sqlite.SqliteConnection connection)
+        {
+            if (wasSignedUp)
+            {
+                Data.RuntimeCharacter character = new Data.RuntimeCharacter();
+                character.tag = "Bot";
+                character.selected = true;
+                long characterID = Manager.CreateCharacter(accountID, character, connection);
+
+                Data.RuntimeEquipment weapon = new Data.RuntimeEquipment();
+                weapon.tag = "Scar";
+                Manager.CreateEquipment(accountID, characterID, weapon);
+
+                character = new Data.RuntimeCharacter();
+                character.tag = "Soldier";
+                character.selected = false;
+                characterID = Manager.CreateCharacter(accountID, character, connection);
+
+                weapon = new Data.RuntimeEquipment();
+                weapon.tag = "AKM";
+                Manager.CreateEquipment(accountID, characterID, weapon);
+            }
+        }
+
+        public static (Data.PurchaseResult, int) OverridePurchase(long accountID, int itemCategory, int itemID, int itemLevel, int currencyID, Microsoft.Data.Sqlite.SqliteConnection connection)
+        {
+            uint price = 99999;
+            Data.PurchaseResult result = Data.PurchaseResult.Unknown;
+
+            /*
+            bool purchased = false;
+            if (itemCategory == weapon && itemID == excalibur && itemLevel == 1)
+            {
+                if(currencyID == coins)
+                {
+                    price = 12345;
+                    purchased = Manager.SpendCoins(accountID, price);
+                }
+            }
+            if(purchased)
+            {
+                // Add item here. For example:
+                Data.RuntimeEquipment sword = new Data.RuntimeEquipment();
+                sword.name = "Excalibur";
+                sword.tag = "excalibur";
+                sword.prefabID = 0;
+                sword.weight = 4.8;
+                sword.damage = 25;
+                sword.level = itemLevel;
+                Manager.CreateEquipment(accountID, 0, sword);
+            }
+            else
+            {
+                result = Data.PurchaseResult.InsufficientFunds;
+            }
+            */
+
+            return (result, (int)price);
+        }
+
+        public static (int, int) OverrideMatchmaking(int gameID, int mapID)
+        {
+            int teamsPerMatch = 2;
+            int playersPerTeam = 1;
+            return (teamsPerMatch, playersPerTeam);
+        }
+
+        public static void OverrideGameInitialData(ref Data.RuntimeGame data, Microsoft.Data.Sqlite.SqliteConnection connection)
+        {
+
+        }
+
+        public static void OnGameFinished(Data.Game game)
+        {
+
+        }
+
+        public static void OnNetcodeGameResultReceived(Data.RuntimeResult result)
+        {
+
+        }
+
+    }
+}
